bcf30661dc6da7437a5809129ca80062aea4aef5
Erez <erez@365Scores.com>
2021-03-16 18:07:53 +0200

    MiniCalc solution

---
+++ MiniCalc/MiniCalc/Controllers/CalcController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniCalc.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalcController : ControllerBase
+    {
+        private readonly ILogger<CalcController> _logger;
+
+        public CalcController(ILogger<CalcController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("add")]
+        public int Get(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
